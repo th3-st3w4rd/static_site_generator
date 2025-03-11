fcd88c61a55048964224d0c951c8de85790f8628
th3-st3w4rd <ChristianS.Green@gmail.com>
2025-03-11 08:11:34 -0400

    Added logging in main, just started to implement file manipulation/deletion.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,36 @@
+import os
+import logging
+
+from pathlib import Path
 from textnode import TextNode, TextType
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
+logging.basicConfig(
+    encoding="utf-8",
+    level="DEBUG",
+    format="%(levelname)s - %(asctime)s | %(message)s",
+    filename="/var/log/static_site_bootdev.log",
+    filemode="w",
+    datefmt="%m/%d/%Y %I:%M:S %P"
+)
+
+working_dir = Path(__file__)
+source_dir = working_dir.parent.parent.joinpath("static")
+target_dir = working_dir.parent.parent.joinpath("public")
 
 def main():
-    test = TextNode(text="Thit is a text node",text_type="bold",url="https://boot.dev")
+    logging.info("Starting main function.")
+    initialize()
+
+
+def initialize():
+    # 1. Delete all contents of destination directory.
+    print(os.listdir())
+    os.remove(target_dir)
+    # 2. Copy all files and subdirectories & nest files
+
+    # 3. Add logging.
+    pass
 
 
 if __name__ == "__main__":
