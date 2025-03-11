423165cbba467fe3ecbf93efc3a75e48c7e0483e
th3-st3w4rd <ChristianS.Green@gmail.com>
2025-03-11 08:24:53 -0400

    Added a cleanup at run function.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import logging
+import shutil
 
 from pathlib import Path
 from textnode import TextNode, TextType
@@ -11,7 +12,7 @@ logging.basicConfig(
     format="%(levelname)s - %(asctime)s | %(message)s",
     filename="/var/log/static_site_bootdev.log",
     filemode="w",
-    datefmt="%m/%d/%Y %I:%M:S %P"
+    datefmt="%m/%d/%Y %I:%M:%S %P"
 )
 
 working_dir = Path(__file__)
@@ -24,13 +25,17 @@ def main():
 
 
 def initialize():
+    logging.info("Starting to initialize project")
     # 1. Delete all contents of destination directory.
-    print(os.listdir())
-    os.remove(target_dir)
+    if target_dir.exists():
+        shutil.rmtree(target_dir)
+        logging.debug(f"Removing {target_dir}.")
     # 2. Copy all files and subdirectories & nest files
-
+    if not target_dir.exists():
+        shutil.copytree(source_dir, target_dir)
+        logging.debug(f"Copy from source to target location.")
     # 3. Add logging.
-    pass
+    
 
 
 if __name__ == "__main__":
